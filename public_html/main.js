210424c7e27913a3962a2a1cc3a94aa7c6f68a75
lavrovso <2slavrov@gmail.com>
2019-08-23 14:02:00 +0300

    3rd commit

---
+++ public_html/main.js
@@ -0,0 +1,40 @@
+import {Ball} from "./Ball.js";
+
+const BOX = document.getElementById("box");
+const BALL_SIZE = 40;
+const NUM_OF_BALLS = 20;
+
+function getRandPos() {
+    var x = (parseInt(BOX.style.width) - BALL_SIZE) * Math.random();
+    var y = (parseInt(BOX.style.height) - BALL_SIZE) * Math.random();
+    return [Math.round(x), Math.round(y)];
+}
+
+function getRandVel(v) {
+    var phi = 2 * Math.PI * Math.random();
+    return [v * Math.cos(phi), v * Math.sin(phi)];
+}
+
+function getRandCol() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var balls = new Array();
+
+for (var i = 0; i < NUM_OF_BALLS; i++) {
+    var pos = getRandPos();
+    var vel = getRandVel(3);
+    var col = getRandCol();
+    var b = new Ball("b" + i, pos[0], pos[1], vel[0], vel[1], col, BALL_SIZE, BOX);
+    balls.push(b);
+  }
+
+for (var i in balls) {
+    balls[i].start();
+}
+
